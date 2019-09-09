88798b6196091e79ea17c9edd5eaa3ce31025ad9
Laurent ARCHIMEDE (ANEO) <larchimede@aneo.fr>
2019-09-09 14:14:22 +0200

    introduce the item wrapper (feature envy smell)

---
+++ Java/src/main/java/com/gildedrose/ItemWrapper.java
@@ -0,0 +1,60 @@
+package com.gildedrose;
+
+public class ItemWrapper {
+    private final Item item;
+
+    public ItemWrapper(Item item) {
+        this.item = item;
+    }
+
+    public void updateQuality() {
+        if (!item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality -= 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality += 1;
+
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality += 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn -= 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality -= 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality += 1;
+                }
+            }
+        }
+    }
+}
