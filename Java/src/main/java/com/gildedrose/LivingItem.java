87a091a4cfef026bb0abcc79d2a8ac21daab5b0e
Laurent ARCHIMEDE (ANEO) <larchimede@aneo.fr>
2019-09-09 17:39:41 +0200

    lift-up conditional refactoring

--- Java/src/main/java/com/gildedrose/LivingItem.java
+++ Java/src/main/java/com/gildedrose/LivingItem.java
@@ -13,33 +13,28 @@ class LivingItem {
     }
 
     private void updateQuality() {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                decreaseQuality();
-            }
-        } else {
+        if (item.name.equals("Aged Brie")) {
             increaseQuality();
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 10) {
-                    increaseQuality();
-                }
-                if (item.sellIn < 5) {
-                    increaseQuality();
-                }
-            }
-        }
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality();
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
+            if (item.sellIn < 0) {
                 increaseQuality();
+            }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQuality();
+            if (item.sellIn < 10) {
+                increaseQuality();
+            }
+            if (item.sellIn < 5) {
+                increaseQuality();
+            }
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            // Nothing to do
+        } else {
+            decreaseQuality();
+            if (item.sellIn < 0) {
+                decreaseQuality();
             }
         }
     }
