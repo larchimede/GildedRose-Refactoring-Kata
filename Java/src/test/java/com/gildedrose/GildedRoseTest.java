f1940bfbae1018b98b10b5306358b45025e662f1
Laurent ARCHIMEDE (ANEO) <larchimede@aneo.fr>
2019-09-09 14:12:36 +0200

    add 100% coverage using a golden master

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,16 +1,26 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.legacycode.Range;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] qualities = {0, 1, -1, 49, 50, 51};
+        Integer[] sellins = Range.get(-1, 15);
+        CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities, sellins);
+    }
+
+    private String doStuff(String name, int quality, int sellIns) {
+        Item[] items = new Item[]{new Item(name, sellIns, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        Approvals.verify("foo");
+        return Arrays.stream(items).findFirst().get().toString();
     }
 
 }
